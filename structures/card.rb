e3ce568fc384a6026f33f88eb28516a2b0526e6b
Vitalii Lazebnyi <ext-vitalii.lazebnyi@here.com>
2018-10-11 13:35:20 +0300

    Card entiry

--- structures/card.rb
+++ structures/card.rb
@@ -4,17 +4,46 @@
 #
 # Suits: 'Diamond', 'Spade', 'Club', 'Heart'
 class Card
-  attr_reader :suit
-  attr_reader :points
-  attr_reader :name
+  SUITS = {Diamond: "\u25C6",
+           Spade:   "\u2660",
+           Club:    "\u2663",
+           Heart:   "\u2665"}.freeze
+  NAMES = {Six:          6,
+           Seven:        7,
+           Eight:        8,
+           Nine:         9,
+           Ten:         10,
+           Jack:         2,
+           Queen:        3,
+           Кing:         4,
+           Ace:          5,
+           'Wild card': 11}.freeze
 
-  def initialize(suit, points, name)
+  def self.SUITS_LIST
+    SUITS.keys
+  end
+
+  def self.NAMES_LIST
+    NAMES.keys
+  end
+
+  attr_reader :suit
+
+  def initialize(suit, name)
     @suit   = suit
-    @points = points
     @name   = name
   end
 
-  def to_s
+  def name
+    return points if points.between?(6, 10)
+    @name
+  end
 
+  def points
+    NAMES[@name]
+  end
+
+  def to_s
+    "#{name}#{SUITS[suit]}"
   end
 end
